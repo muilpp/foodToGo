3513284c459f1e0898a5ee35241a25a225a10eb5
Marc <muilpp@gmail.com>
2021-05-08 20:58:30 +0200

    Compile build for all platforms

--- compile-build.sh
+++ compile-build.sh
@@ -1,2 +1,3 @@
-env GOOS=linux go build -o "food2Go";
-
+env GOOS=linux go build -o "food2GoLinux";
+env GOOS=darwin GOARCH=amd64 go build -o "food2GoMac";
+env GOOS=windows GOARCH=amd64 go build -o "food2GoWindows";
